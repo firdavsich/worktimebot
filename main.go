0fa68aac90bf7f76228fec449e5dec8017b882ee
Firdavs Murodov <firdavs.murodov@gmail.com>
2021-09-14 15:08:58 +0500

    skip weekend

--- main.go
+++ main.go
@@ -24,19 +24,21 @@ func main() {
 	}
 
 	for range time.Tick(time.Minute) {
+		// if not weekend
+		if time.Now().Weekday() > 1 {
+			if time.Now().Hour() == startTime && !startedWork {
+				startedWork = true
+				if err := bot.Send(chatID, "Start workday"); err != nil {
+					log.Println(err)
+				}
 
-		if time.Now().Hour() == startTime && !startedWork {
-			startedWork = true
-			if err := bot.Send(chatID, "Start workday"); err != nil {
-				log.Println(err)
+			} else if time.Now().Hour() == endTime && startedWork {
+				startedWork = false
+				if err := bot.Send(chatID, "End workday"); err != nil {
+					log.Println(err)
+				}
+
 			}
-
-		} else if time.Now().Hour() == endTime && startedWork {
-			startedWork = false
-			if err := bot.Send(chatID, "End workday"); err != nil {
-				log.Println(err)
-			}
-
 		}
 
 	}
