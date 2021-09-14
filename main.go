db6a84b4b0b6bf72967fc301d4594379c215343b
Firdavs Murodov <firdavs.murodov@gmail.com>
2021-09-14 23:40:45 +0500

    now var for time.Now

--- main.go
+++ main.go
@@ -8,6 +8,8 @@ import (
 )
 
 func main() {
+	var now time.Time
+
 	err := getvars()
 	if err != nil {
 		log.Fatal(err)
@@ -24,15 +26,16 @@ func main() {
 	}
 
 	for range time.Tick(time.Minute) {
+		now = time.Now()
 		// if not weekend
-		if time.Now().Weekday() != time.Saturday && time.Now().Weekday() != time.Sunday {
-			if time.Now().Hour() == startTime && !startedWork {
+		if now.Weekday() != time.Saturday && now.Weekday() != time.Sunday {
+			if now.Hour() == startTime && !startedWork {
 				startedWork = true
 				if err := bot.Send(chatID, "Start workday"); err != nil {
 					log.Println(err)
 				}
 
-			} else if time.Now().Hour() == endTime && startedWork {
+			} else if now.Hour() == endTime && startedWork {
 				startedWork = false
 				if err := bot.Send(chatID, "End workday"); err != nil {
 					log.Println(err)
